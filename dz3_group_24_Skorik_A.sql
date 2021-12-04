fdb5e4c789fe4a6ca503c67e4e6455bb5ba053b1
scalser <scalserstv@yandex.ru>
2021-12-04 17:29:27 +0300

    dz 3 sql full +

--- dz3_group_24_Skorik_A.sql
+++ dz3_group_24_Skorik_A.sql
@@ -1,12 +1,10 @@
 --  1. Вывести всех работников чьи зарплаты есть в базе, вместе с зарплатами.
-select role_name, monthly_salary from roles join roles_employee on roles.id = role_id
-join employee_salary on roles_employee.employee_id = employee_salary.employee_id
+select employee_name, monthly_salary from employees join employee_salary on employees.id = employee_salary.employee_id
 join salary on salary_id = salary.id;
 
 -- 2. Вывести всех работников у которых ЗП меньше 2000.
-select role_name, monthly_salary from roles join roles_employee on roles.id = role_id
-join employee_salary on roles_employee.employee_id = employee_salary.employee_id
-join salary on salary_id = salary.id where monthly_salary <2000;
+select employee_name, monthly_salary from employees join employee_salary on employees.id = employee_salary.employee_id
+join salary on salary_id = salary.id where monthly_salary<2000;
 
 -- 3. Вывести все зарплатные позиции, но работник по ним не назначен. (ЗП есть, но не понятно кто её получает.)
 select role_name, monthly_salary from roles join roles_employee on roles.id = role_id
@@ -19,9 +17,8 @@ join employee_salary on roles_employee.employee_id = employee_salary.employee_id
 right join salary on salary_id = salary.id where role_name is null and monthly_salary <2000;
 
 -- 5. Найти всех работников кому не начислена ЗП.
-select role_name, monthly_salary from roles join roles_employee on roles.id = role_id
-left join employee_salary on roles_employee.employee_id = employee_salary.employee_id
-left join salary on salary_id = salary.id where  monthly_salary is null;
+select employee_name, monthly_salary from employees left join employee_salary on employees.id = employee_salary.employee_id
+left join salary on salary_id = salary.id  where  monthly_salary is null;
 
 -- 6. Вывести всех работников с названиями их должности.
 select employee_name, role_name from employees join roles_employee on employees.id = employee_id
